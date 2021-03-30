d2fcc8d353b33cd8a281b3d5c6e93147f7a18e80
scorsaro <salvatore.corsaro@live.com>
2021-03-30 22:25:24 +0200

    final

---
+++ src/main/java/com/scorsaro/hellograalvmdemo/HelloGraalvmApp.java
@@ -0,0 +1,13 @@
+package com.scorsaro.hellograalvmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGraalvmApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGraalvmApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/scorsaro/hellograalvmdemo/HelloGraalvmAppTests.java
@@ -0,0 +1,13 @@
+package com.scorsaro.hellograalvmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGraalvmAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
